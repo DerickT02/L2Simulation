c39ed14f6c2f1bfb7b390c6fa28ea1712cb1260f
DerickT02 <derickt18@outlook.com>
2023-04-05 11:09:43 -0700

    Added planets to screen

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
 import turtle
+
+if __name__ == "__main__":
+    screen = turtle.getscreen()
+    #sun
+    sun = turtle.Turtle()
+    
+    #moon
+    moon = turtle.Turtle()
+  
+    #earth
+    earth = turtle.Turtle()
+
+
+    #draw masses on screen
+    earth.pencolor("blue")
+    earth.fillcolor("blue")
+    earth.dot(50)
+
+    moon.penup()
+    moon.setpos(50, 50)
+    moon.fillcolor("#CCCCCC")
+    moon.pencolor("#CCCCCC")
+    moon.dot(20)
+
+    sun.penup()
+    sun.setpos(0, -200)
+    sun.pencolor("yellow")
+    sun.fillcolor("yellow")
+    sun.dot(100)
+
+
+    #initialize screen
+    screen.listen()
+    screen.mainloop()
+    
