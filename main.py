00c704de779b6dfdaabcd81c86ae2c06589cd4e2
DerickT02 <derickt18@outlook.com>
2023-04-08 07:48:13 -0700

    Implemented gravitational force equation and kinematics to make earth orbit around the sun

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import turtle
-from screen import Screen
+from solarSystem import SolarSystem
 from massObject import MassObject
 
 
 if __name__ == "__main__":
-    mainScreenObj = Screen()
-    mainScreen = mainScreenObj.getScreen()
-    #test
-    sun = MassObject(0, -100, "yellow", 100)
-    earth = MassObject(0, 0, "blue", 20)
-    earth.changePosition(0, -100)
-    mainScreen.mainloop()
+    solarSystem = SolarSystem(1400, 900)
+    
+    sun = MassObject("yellow", 10000, solarSystem)
+    earth = MassObject("blue", 1, solarSystem, (-270, 0), (0, 5))
+    moon = MassObject("gray", 0.01, solarSystem, (-272, 0), (0, 4))
+    
+
+    while True:
+        solarSystem.accelerationDueToGravity(sun, earth)
+        solarSystem.accelerationDueToGravity(earth, moon)
+        
+        solarSystem.updateAll()
+
+    
+
+    
+ 
