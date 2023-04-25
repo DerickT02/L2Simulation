6dbc1f106fbb9b63e4da2c7bcea3c2540395f857
DerickT02 <derickt18@outlook.com>
2023-04-25 16:12:19 -0700

    Moon moves around Earth

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import turtle
+import math
 from solarSystem import SolarSystem
 from massObject import MassObject
+from moon import Moon
 
 
 if __name__ == "__main__":
-    solarSystem = SolarSystem(1400, 900)
-    
-    sun = MassObject("yellow", 10000, solarSystem)
-    earth = MassObject("blue", 1, solarSystem, (-270, 0), (0, 5))
-    moon = MassObject("gray", 0.01, solarSystem, (-272, 0), (0, 4))
+    solarSystem = SolarSystem(1400, 900, 0, 0)
+
+    #massObject(color, mass, solarSystem, position, velocity)
+    sun = MassObject("yellow", 20000, solarSystem)
+    earth = MassObject("blue", 6, solarSystem, (-470, 0), (0, 5))
+    moon = Moon("gray", 10, 45, 1)
     
 
     while True:
         solarSystem.accelerationDueToGravity(sun, earth)
-        solarSystem.accelerationDueToGravity(earth, moon)
-        
+        moon.move()
+        moon.goto(earth.xcor() + moon.orbit_radius * math.cos(math.radians(moon.angle)),
+              earth.ycor() + moon.orbit_radius * math.sin(math.radians(moon.angle)))
+   
         solarSystem.updateAll()
+
+
+        
 
     
 
